30fc7251b0e727cfb5507682dd7fb573d6a1b5cf
d_barnett <domoniquebarnett7@gmail.com>
2015-11-09 04:46:40 +0000

    new javascript file

--- fifteen.js
+++ fifteen.js
@@ -9,6 +9,7 @@ window.onload = function(){
 	var backgroundTop = 0;
 	var topX;
 	var leftY;
+	var shuffle;
 	for(var i =0; i < pieces.length; i++){
 		pieces[i].className = "puzzlepiece";
 		pieces[i].style.top =  positionTop + "px";
@@ -27,4 +28,37 @@ window.onload = function(){
 		}
 	}
 	
+	for ( i in pieces){//gets pieces to move
+	  pieces[i].onclick=function movepiece(){
+	    topX = parseInt(this.style.top);
+	    leftY = parseInt(this.style.left);
+		if (topX == topEmptyspace && leftY == (leftEmptyspace-100) || topX == topEmptyspace && leftY == (leftEmptyspace+100) || topX == (topEmptyspace-100) && leftY == leftEmptyspace || topX == (topEmptyspace+100) && leftY == leftEmptyspace){
+		this.style.top = topEmptyspace + "px";
+		this.style.left = leftEmptyspace + "px";
+		topEmptyspace = topX;
+		leftEmptyspace = leftY;
+		this.className("movablepiece");
+		}
+	  }
+	}
+	//necessary for shuffle function of puzzle
+	//var shuffling = Math.floor (Math.random () * 4);
+	
+	//attempt at creating selection box. not complete
+	/*var userChoice = prompt("please choose your puzzle image. Option1, option2, option3,option4");
+switch (userChoice) {
+    case 'option1':
+    pieces[i].style.backgroundImage="url('http://b4.pinger.pl/5647fd9826e94582f73f9def5104c2bf/zdjecie_wykonawcypora_na_przygode.jpg')";
+        break;
+    case 'option2':
+    pieces[i].style.backgroundImage="url('http://e27caaa35df580732627-5fa18836a2ae6b5e7c49abcc89b20237.r67.cf1.rackcdn.com/2519411_888a1420_m.jpeg')";
+        break;
+    case 'option3':
+    pieces[i].style.backgroundImage="url('http://www.24horas.cl/incoming/oye-arnoldjpg-1692191/ALTERNATES/w460h260/Oye-Arnold.jpg')";
+        break;
+    case 'option4':
+    pieces[i].style.backgroundImage="url('http://4.bp.blogspot.com/-UlCFf9Zt6ug/T0L4KjcxaGI/AAAAAAAAA5A/gIRm127NaAg/s400/gatos_famosos_20.jpg')";
+        break;
+    default:
+    pieces[i].style.backgroundImage="url('background.jpg')";*/
 };
